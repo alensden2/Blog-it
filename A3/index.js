2ee1841dfa399015137bf13c16cee2db8871a4b9
alensden2 <alensanjohn1999@gmail.com>
2023-07-07 22:22:20 -0300

    Final Assignment Submission

--- A3/index.js
+++ A3/index.js
@@ -6,6 +6,7 @@ const app = express();
 
 const PORT = 80;
 
+// connection params
 const connection = mysql.createConnection({
     host: "assignment2db-instance-1.cbumd6b2hpli.us-east-1.rds.amazonaws.com",
     port: "3306",
@@ -13,29 +14,35 @@ const connection = mysql.createConnection({
     password: "rootroot"
 });
 
+// connects to the databse
 connection.connect((err) => {
+    // throws err incase of exception
     if (err) {
         console.error("Failed to connect to the database:", err);
         return;
     }
+
+    // create database
     connection.query("CREATE DATABASE IF NOT EXISTS assignment2", (err) => {
         if (err) {
             console.error("Failed to create database:", err);
             return;
         }
         console.log("Database created");
+        // use the newly created database
         connection.query("USE assignment2", (err) => {
             if (err) {
                 console.error("Failed to select database:", err);
                 return;
             }
             console.log("Database selected");
+            // create the product table
             createProductsTable();
         });
     });
 });
 
-// Create the products table if it doesn't exist
+// creates the products table
 function createProductsTable() {
     const query = `
         CREATE TABLE IF NOT EXISTS products (
@@ -55,10 +62,12 @@ function createProductsTable() {
 
 app.use(bodyParser.json());
 
+// test endpoint
 app.get("/", (req, res) => {
     res.send("<h1>EC2 hello!</h1>");
 });
 
+// stores product
 app.post("/store-products", (req, res) => {
     const { products } = req.body;
     if (!products || !Array.isArray(products)) {
@@ -88,6 +97,7 @@ app.post("/store-products", (req, res) => {
         });
 });
 
+// lists products
 app.get("/list-products", (req, res) => {
     const selectQuery = "SELECT name, price, availability FROM products";
     connection.query(selectQuery, (err, results) => {
@@ -107,6 +117,7 @@ app.get("/list-products", (req, res) => {
 
 createProductsTable();
 
+// app listens on port 80
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 });
